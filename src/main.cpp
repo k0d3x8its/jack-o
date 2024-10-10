076046031d9daaa45b12df85946164e52acdea1c
K0d3x 8its <k0d3x@pme.com>
2024-10-09 21:30:05 -0400

    added comments for MCU

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
-#include <Arduino.h>
+/******************************************************************************
+* + Project: Jack-O & The Lanterns                                            *
+* + Version: 0.1.0a                                                           *
+* + Description: Animatronic singing pumpkin trio                             *
+* + Board: Feather Huzzah ESP8266                                             *
+* + Sensor: N/A                                                               *
+* + Module: SG90 Micro Servo                                                  *
+* + Author: K0d3x 8its                                                        *
+* + Date: October 9th, 2024                                                   *
+* + License: MIT                                                              *
+*******************************************************************************
+  + Inspired by: Duckman | https://busyducks.com/ascii-art-arduino-pinouts/
+  + Designed by: K0d3x   | https://github.com/k0d3x8its
+*******************************************************************************
+*                           ----[      ]-----             * VUSB = VIN | 5VDC *
+*                      [ ]-|RST          ____|            *********************
+*   MAX output 400mA = [ ]-|3V3         | __ |                                *
+*                      [ ]-|NC          | __ |-[ ] = 3.7V Battery             *
+*                      [ ]-|GND         |____|                                *
+*                ADC = [ ]-|A0            BAT|-[ ] = LiPo                     **************
+*                      [ ]-|NC             EN|-[ ] = connect GND to disable 3.3V regulator *
+*                      [ ]-|NC           VUSB|-[ ] = VBUS                     * ************
+*                      [ ]-|NC          ~ D14|-[ ] = SCK                      * 
+*                      [ ]-|NC          ~ D12|-[ ] = MISO                     * 
+*                      [ ]-|NC          ~ D13|-[ ] = MOSI                     * 
+*                SCK = [ ]-|D14 ~  *    ~ D15|-[ ]                            *
+*             ~ MOSI = [ ]-|D13  Feather ~ D0|-[ ] = Board LED/Bootloader     * 
+*             ~ MISO = [ ]-|D12  Huzzah ~ D16|-[ ] = WAKE                     *
+*                RX--> [ ]-|D3 ~         ~ D2|-[ ]                            * 
+*                TX <--[ ]-|D1 ~         ~ D5|-[ ] = SCL                      * 
+*                 EN = [ ]-|CHPD         ~ D4|-[ ] = SDA                      * 
+*                           \_______________/                                 *                           
+*                                                                             *                               
+*******************************************************************************
+    ******************     Feather Huzzah Overview      ******************  
+*******************************************************************************
+   ****** https://learn.adafruit.com/adafruit-feather-huzzah-esp8266 ******
+*******************************************************************************
+*******************************************************************************
+         ******************  Example Datasheet  ******************
+*******************************************************************************
+               *** https://www.website.com/path/to/datasheet ***
+******************************************************************************/
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
